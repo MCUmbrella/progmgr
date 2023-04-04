ee1c3900a72d84ca95e9abc42c7ec09b6b548194
MCUmbrella <mcumbrella@protonmail.com>
2023-04-04 23:49:10 +0800

    complete ProgramData

--- src/main/java/vip/floatationdevice/progmgr/DataManager.java
+++ src/main/java/vip/floatationdevice/progmgr/DataManager.java
@@ -1,5 +1,19 @@
 package vip.floatationdevice.progmgr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
 public class DataManager
 {
+    private static final Logger l = LoggerFactory.getLogger(DataManager.class);
+    private static LinkedList<ProgramData> data;
+
+    static class ProgramDataContainer implements Serializable
+    {
+        public static final long serialVersionUID = 0L;
+        public LinkedList<ProgramData> data;
+    }
 }
--- src/main/java/vip/floatationdevice/progmgr/Main.java
+++ src/main/java/vip/floatationdevice/progmgr/Main.java
@@ -1,13 +1,50 @@
 package vip.floatationdevice.progmgr;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 @SpringBootApplication
 public class Main
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Main.class, args);
+        //SpringApplication.run(Main.class, args);
+        ProgramData p = ProgramData.fromString("""
+                {
+                    "type": "戏曲",
+                    "name": "三顾茅庐",
+                    "point": "主打就是一个颠覆三观！",
+                    "actors": "张飞，周瑜，诸葛亮"
+                }""");
+        for(String s : p.toMap().keySet())
+            System.out.println(s + ": " + p.toMap().get(s));
+        try
+        {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("program"));
+            oos.writeObject(p);
+            oos.flush();
+            oos.close();
+            System.out.println("saved");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        try
+        {
+            ProgramData p2 = (ProgramData) new ObjectInputStream(new FileInputStream("program")).readObject();
+            System.out.println("loaded");
+            for(String s : p.toMap().keySet())
+                System.out.println(s + ": " + p.toMap().get(s));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
