cc7d92e873526ecf6a4908426a77653924ec6738
MCUmbrella <mcumbrella@protonmail.com>
2023-04-09 12:03:00 +0800

    Update DataReadController.java

--- src/main/java/vip/floatationdevice/progmgr/controller/DataReadController.java
+++ src/main/java/vip/floatationdevice/progmgr/controller/DataReadController.java
@@ -2,17 +2,16 @@ package vip.floatationdevice.progmgr.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import vip.floatationdevice.progmgr.DataManager;
 import vip.floatationdevice.progmgr.data.CommonMapResult;
 
 import java.util.HashMap;
-import java.util.Map;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static vip.floatationdevice.progmgr.CommonUtil.isBlank;
 
 @SuppressWarnings("unused")
 @RestController
@@ -25,67 +24,50 @@ public class DataReadController
         return new CommonMapResult(0, "OK", DataManager.getDataCount());
     }
 
-    @RequestMapping(value = "/get/programList", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @RequestMapping("/get/programList")
     @ResponseBody
-    public CommonMapResult actionGetPagedData(@RequestBody Map<String, Object> requestBody, HttpServletRequest request, HttpServletResponse response) throws Exception
+    public CommonMapResult actionGetPagedData(HttpServletRequest request, HttpServletResponse response, @RequestParam(name = "pageNum") Integer pageNum) throws Exception
     {
-        if(requestBody.get("pageNum") == null || !(requestBody.get("pageNum") instanceof Integer))
-        {
-            response.sendError(400, "Missing parameter");
-            return null;
-        }
-        else
-        {
-            HashMap<String, Object> programResults = new HashMap<>();
-            programResults.put("programResults", DataManager.getPagedData((int) requestBody.get("pageNum")));
-            return new CommonMapResult(0, "OK", programResults);
-        }
+        HashMap<String, Object> programResults = new HashMap<>();
+        programResults.put("programResults", DataManager.getPagedData(pageNum));
+        return new CommonMapResult(0, "OK", programResults);
     }
 
-    @RequestMapping(value = "/get/program", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @RequestMapping("/get/program")
     @ResponseBody
-    public CommonMapResult actionGetData(@RequestBody Map<String, Object> requestBody, HttpServletRequest request, HttpServletResponse response) throws Exception
+    public CommonMapResult actionGetData(HttpServletRequest request, HttpServletResponse response, @RequestParam(name = "id") Integer id) throws Exception
     {
-        if(requestBody.get("id") == null || !(requestBody.get("id") instanceof Integer))
+        if(DataManager.hasData(id))
         {
-            response.sendError(400, "Missing parameter");
-            return null;
+            HashMap<String, Object> program = new HashMap<>();
+            program.put("program", DataManager.getData(id));
+            return new CommonMapResult(0, "OK", program);
         }
-        if(!DataManager.hasData((int) requestBody.get("id")))
-        {
-            response.sendError(404, "No such program");
-            return null;
-        }
-        HashMap<String, Object> program = new HashMap<>();
-        program.put("program", DataManager.getData((int) requestBody.get("id")));
-        return new CommonMapResult(0, "OK", program);
+        response.sendError(404, "Program with ID " + id + " not exists");
+        return null;
     }
 
-    @RequestMapping(value = "/get/search", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @RequestMapping("/get/search")
     @ResponseBody
-    public CommonMapResult actionFindData(@RequestBody Map<String, Object> requestBody, HttpServletRequest request, HttpServletResponse response) throws Exception
+    public CommonMapResult actionFindData(
+            HttpServletRequest request, HttpServletResponse response,
+            @RequestParam(name = "type", required = false) String type,
+            @RequestParam(name = "num", required = false) Integer num,
+            @RequestParam(name = "name", required = false) String name
+    ) throws Exception
     {
-        Object type = requestBody.get("type"), num = requestBody.get("num"), name = requestBody.get("name");
         if(type == null && num == null && name == null)
         {
             response.sendError(400, "Missing parameter");
             return null;
         }
-        if((type != null && !(type instanceof String)) ||
-                (num != null && !(num instanceof Integer)) ||
-                (name != null && !(name instanceof String))
-        )
-        {
-            response.sendError(400, "Invalid parameter type");
-            return null;
-        }
-        if(name != null && ((String) name).isBlank())
+        if(name != null && name.isBlank())
         {
             response.sendError(400, "Name is empty");
             return null;
         }
         HashMap<String, Object> programSearchResults = new HashMap<>();
-        programSearchResults.put("programSearchResults", DataManager.findData((String) type, (Integer) num, (String) name));
+        programSearchResults.put("programSearchResults", DataManager.findData(type, num, name));
         return new CommonMapResult(0, "OK", programSearchResults);
     }
 }
