e7f024e5d85fe98ac348c4d20def0b2058e29008
MCUmbrella <mcumbrella@protonmail.com>
2023-04-17 21:06:12 +0800

    start working on annotation based validation

--- src/main/java/vip/floatationdevice/progmgr/controller/ErrorJsonController.java
+++ src/main/java/vip/floatationdevice/progmgr/controller/ErrorJsonController.java
@@ -16,7 +16,8 @@ public class ErrorJsonController implements ErrorController
     {
         return new CommonMappedResult(
                 (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE),
-                (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE)
+                (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE),
+                request.getAttribute(RequestDispatcher.ERROR_EXCEPTION)
         );
     }
 }
